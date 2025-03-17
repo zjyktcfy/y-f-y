09672ff0145e7f221dc8b95e8711b24fa5e90ec0
zjyktcfy <zjykfy@foxmail.com>
2025-03-17 15:44:04 +0800

    Update CaijiYinniip.sh

--- speedtest/CaijiYinniip.sh
+++ speedtest/CaijiYinniip.sh
@@ -34,7 +34,7 @@ case $city_choice in
         channel_key="印尼"
 #        url_fofa=$(echo  '"udpxy" && country="ID" && org="PT. Total Info Kharisma" && protocol="http"' | base64 |tr -d '\n')
 #        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJJRCIgJiYgb3JnPSJQVC4gVG90YWwgSW5mbyBLaGFyaXNtYSIgJiYgcHJvdG9jb2w9Imh0dHAi&page_size=20"$url_fofa
-        url_fofa="https://li.yangfeiyue.us.kg/Yinni_PT.txt"$url_fofa
+        url_fofa="https://li.yangfeiyue.ip-ddns.com/Yinni_PT.txt"$url_fofa
         ;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
