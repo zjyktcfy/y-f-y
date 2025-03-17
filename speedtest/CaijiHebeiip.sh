6cbc606e70b256e0c1e824f64131834b579e6c34
zjyktcfy <zjykfy@foxmail.com>
2025-03-17 15:42:45 +0800

    Update CaijiHebeiip.sh

--- speedtest/CaijiHebeiip.sh
+++ speedtest/CaijiHebeiip.sh
@@ -34,7 +34,7 @@ case $city_choice in
         channel_key="河北"
 #        url_fofa=$(echo  '"udpxy" && country="CN" && region="Hebei" && org="CHINA UNICOM China169 Backbone" && protocol="http"' | base64 |tr -d '\n')
 #        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJIZWJlaSIgJiYgb3JnPSJDSElOQSBVTklDT00gQ2hpbmExNjkgQmFja2JvbmUiICYmIHByb3RvY29sPSJodHRwIg%3D%3D&page_size=20"$url_fofa
-        url_fofa="https://fu.yangfeiyue.us.kg/Hebei_liantong.txt"$url_fofa
+        url_fofa="https://fu.yangfeiyue.ip-ddns.com/Hebei_liantong.txt"$url_fofa
         ;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
