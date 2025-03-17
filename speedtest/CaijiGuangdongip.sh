892c7a5970eb1e5a0740ecd9cc1eb8cc8252cd37
zjyktcfy <zjykfy@foxmail.com>
2025-03-17 15:42:33 +0800

    Update CaijiGuangdongip.sh

--- speedtest/CaijiGuangdongip.sh
+++ speedtest/CaijiGuangdongip.sh
@@ -34,7 +34,7 @@ case $city_choice in
         channel_key="广东"
 #        url_fofa=$(echo  '"udpxy" && country="CN" && region="Guangdong" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
 #        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJHdWFuZ2RvbmciICYmIG9yZz0iQ2hpbmFuZXQiICYmIHByb3RvY29sPSJodHRwIg%3D%3D&page_size=20"$url_fofa
-        url_fofa="https://fo.yangfeiyue.us.kg/Guangdong_dianxin.txt"$url_fofa
+        url_fofa="https://fo.yangfeiyue.ip-ddns.com/Guangdong_dianxin.txt"$url_fofa
         ;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
