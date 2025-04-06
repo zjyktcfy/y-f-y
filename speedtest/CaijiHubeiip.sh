cec4586c4aeb70b46b1511cc0d8b36b701e79887
zjyktcfy <zjykfy@foxmail.com>
2025-04-06 12:57:36 +0800

    Update CaijiHubeiip.sh

--- speedtest/CaijiHubeiip.sh
+++ speedtest/CaijiHubeiip.sh
@@ -34,7 +34,7 @@ case $city_choice in
         channel_key="湖北"
 #        url_fofa=$(echo  '"udpxy" && country="CN" && region="Hubei" && org="Chinanet" && protocol="http"' | base64 |tr -d '\n')
 #        url_fofa="https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjb3VudHJ5PSJDTiIgJiYgcmVnaW9uPSJIdWJlaSIgJiYgb3JnPSJDaGluYW5ldCIgJiYgcHJvdG9jb2w9Imh0dHAi&page_size=20"$url_fofa
-        url_fofa="https://fa.yangfeiyue.ip-ddns.com/Hubei_dianxin.txt"$url_fofa
+        url_fofa="https://faa.yangfeiyue.dpdns.org/Hubei_dianxin.txt"$url_fofa
         ;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
