0068302a0d0ee94594cdf132fd212c55ef9a8012
zjyktcfy <zjykfy@foxmail.com>
2025-04-06 13:08:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,64 +1,46 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Tianjin_liantong.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Zhejiang_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Beijing_liantong.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Shanghai_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Guangdong_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Henan_liantong.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Hebei_liantong.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Shanxi_liantong.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Shaanxi_dianxin.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Liaoning_liantong.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Guangxi_dianxin.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Fujian_dianxin.txt",
-    #"https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Sichuan_yidong.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Sichuan_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Jiangsu_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Anhui_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Hubei_dianxin.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Chongqing_liantong.txt",
-    "https://live.yangfeiyue.ip-ddns.com/speedtest/txt/Hunan_dianxin.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Guangdian.txt",
-    "https://cai.yangfeiyue.ip-ddns.com/speedtest/txt/Jiangxi_dianxin.txt",
-    #"https://fei.yangfeiyue.ip-ddns.com/GAT.m3u",
-    "https://fei.yangfeiyue.ip-ddns.com/NEWTV.m3u",
-    #"https://fei.yangfeiyue.ip-ddns.com/Liaoning_liantong.txt",
-    #"https://live.yangfeiyue.ip-ddns.com/speedtest/txt/KR_SK.txt",
-    #"https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Tianjin_liantong.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Zhejiang_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Beijing_liantong.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Shanghai_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Guangdong_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Henan_liantong.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Hebei_liantong.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Shanxi_liantong.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Shaanxi_dianxin.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Liaoning_liantong.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Guangxi_dianxin.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Fujian_dianxin.txt",
+    #"https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Sichuan_yidong.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Sichuan_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Jiangsu_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Anhui_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Hubei_dianxin.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Chongqing_liantong.txt",
+    "https://live1.yangfeiyue.dpdns.org/speedtest/txt/Hunan_dianxin.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Guangdian.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/Jiangxi_dianxin.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/NEW.m3u",
+    #"https://live1.yangfeiyue.dpdns.org/speedtest/txt/KR_SK.txt",
 ]
 
 url_blacklist = [
     "epg.pw/stream/",
-    "103.40.13.71:12390",
-    "[2409:8087:1a01:df::4077]/PLTV/",
-    "8.210.140.75:68",
-    "154.12.50.54",
-    "yinhe.live_hls.zte.com",
-    "8.137.59.151",
-    "[2409:8087:7000:20:1000::22]:6060",
-    "histar.zapi.ip-ddns.com",
-    "test.zapi.ip-ddns.com",
-    "www.tfiplaytv.vip",
-    "dp.sxtv.top",
-    "111.230.30.193",
-    "148.135.93.213:81",
-    "live.goodiptv.club",
-    "iptv.luas.edu.cn",
 ]
 
 announcements = [
     {
         "channel": "公告",
         "entries": [
-            {"name": "直播源均来自网络，不得商用，仅供学习交流使用。", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live.yangfeiyue.ip-ddns.com/tv/武术台.png"},
-            {"name": "更新日期", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live.yangfeiyue.ip-ddns.com/tv/武术台.png"},
-            {"name": None, "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live.yangfeiyue.ip-ddns.com/tv/武术台.png"},
+            {"name": "直播源均来自网络，不得商用，仅供学习交流使用。", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+            {"name": "更新日期", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+            {"name": None, "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
   ]
     }
 ]
 
 epg_urls = [
-    "https://assets.livednow.com/epg.xml",
+    "https://assets.live1dnow.com/epg.xml",
 ]
