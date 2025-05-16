b9da6e31ae3da3f44e2e758a36549b698f04648c
zjyktcfy <zjykfy@foxmail.com>
2025-05-16 19:34:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+ip_version_priority = "ipv6"
+
+source_urls = [
+    "https://zj.yangfeiyue.dpdns.org/txt/浙江电信.txt",
+    "https://sh.yangfeiyue.dpdns.org/txt/上海电信.txt",
+    "https://gd.yangfeiyue.dpdns.org/txt/广东电信.txt",
+    "https://bj.yangfeiyue.dpdns.org/txt/北京联通.txt",
+    "https://tj.yangfeiyue.dpdns.org/txt/天津联通.txt",
+    "https://js.yangfeiyue.dpdns.org/txt/江苏电信.txt",
+    "https://cq.yangfeiyue.dpdns.org/txt/重庆联通.txt",
+    "https://shx.yangfeiyue.dpdns.org/txt/陕西电信.txt",
+    "https://jx.yangfeiyue.dpdns.org/txt/江西电信.txt",
+    "https://ln.yangfeiyue.dpdns.org/txt/辽宁联通.txt",
+]
+
+url_blacklist = [
+    "epg.pw/stream/",
+]
+
+announcements = [
+    {
+        "channel": "公告",
+        "entries": [
+            {"name": "直播源均来自网络，不得商用，仅供学习交流使用。", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+            {"name": "更新日期", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+            {"name": None, "url": "https://live-auth.51kandianshi.com/szgd/csztv4.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+  ]
+    }
+]
+
+epg_urls = [
+    "https://assets.live1dnow.com/epg.xml",
+]
